1c27d446d1624a75bc1d6a6d07aba4d87f776657
Gilang Mentari Hamidy <gilang.hamidy@gmail.com>
2020-03-20 10:09:14 +0100

    Initial baseline for DifferentialFuzzingWASM

---
+++ src/runner-spidermonkey.cpp
@@ -0,0 +1,114 @@
+#include "jsapi.h"
+#include "jsapi-ext.h"
+#include "js/Initialization.h"
+#include "js/CompilationAndEvaluation.h"
+#include "js/SourceText.h"
+#include "js/Conversions.h"
+#include "js/Modules.h"
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <ext/stdio_filebuf.h>
+
+/* The class of the global object. */
+static JSClass global_class = {
+    "global",
+    JSCLASS_GLOBAL_FLAGS,
+    &JS::DefaultGlobalClassOps
+};
+
+std::vector<uint8_t> OpenInput(char const* fileName) {
+  std::ifstream inputFile(fileName);
+  inputFile.seekg(0, std::ios::end);
+  size_t length = inputFile.tellg();
+  inputFile.seekg(0, std::ios::beg);
+  std::vector<uint8_t> buf;
+  buf.resize(length);
+  inputFile.read((char*)buf.data(), length);
+  return buf;
+}
+
+void DumpDisassemble(std::ostream& output, std::vector<uint8_t> const& instructions) {
+  // Create temporary file
+  std::ofstream tempOut("/tmp/instruct.bin");
+  tempOut.write((char*)instructions.data(), instructions.size());
+  tempOut.flush();
+  tempOut.close();
+
+  // Call objdump
+  // Najong hack to get istream lol
+  std::string objdumpCmd { "objdump -D -Mintel,x86-64 -b binary -m i386 /tmp/instruct.bin" };
+  int posix_handle = fileno(::popen(objdumpCmd.c_str(), "r"));
+  __gnu_cxx::stdio_filebuf<char> filebuf(posix_handle, std::ios::in);
+  std::istream is(&filebuf);
+
+  // Print objdump output
+  int i = 0;
+  while(!is.eof()) {
+    std::string line;
+    std::getline(is, line);
+    i++;
+    if(i > 7) // Skip first 7 line
+      output << line << "\n";
+  }
+}
+
+int main(int argc, const char *argv[])
+{
+  if(argc < 2) {
+    std::cerr << "Need file input argument" << std::endl;
+    return 1;
+  }
+
+
+  JS_Init();
+
+  JSContext *cx = JS_NewContext(8L * 1024 * 1024);
+  if (!cx)
+      return 1;
+  if (!JS::InitSelfHostedCode(cx))
+      return 1;
+
+  { // Scope for our various stack objects (JSAutoRequest, RootedObject), so they all go
+    // out of scope before we JS_DestroyContext.
+
+    JS::RealmOptions options;
+    JS::RootedObject global(cx, JS_NewGlobalObject(cx, &global_class, nullptr, JS::FireOnNewGlobalHook, options));
+    if (!global)
+        return 1;
+
+    JS::RootedValue rval(cx);
+
+    { // Scope for JSAutoCompartment
+      JSAutoRealm ac(cx, global);
+      JS::InitRealmStandardClasses(cx);
+
+
+
+      auto inputInstruction = OpenInput(argv[1]);
+      auto res = js::ext::CompileWasmBytes(cx, inputInstruction.data(), inputInstruction.size());
+
+      std::vector<JS::Value> args;
+
+      args.emplace_back(JS::Int32Value(25));
+      args.emplace_back();
+      args.emplace_back(js::ext::CreateBigIntValue(cx, 150ll));
+      args.emplace_back(js::ext::CreateBigIntValue(cx, 300ll));
+      //std::cout << "Param 1: " << args[2].toInt32() << std::endl;
+      bool invokeRes = (*res)["subi64"].Invoke(cx, args);
+
+      std::cout << "Invoke result: " << invokeRes << std::endl;
+      JS::Value& val = args[0];
+      std::cout << "Func result: " << js::ext::GetBigIntValue(val) /*.toInt32()*/ << std::endl;
+      /*
+      for(auto& func : res->functions()) {
+        std::cout << "Function Name: " << func.name << "\nInstructions: \n";
+        DumpDisassemble(std::cout, func.instructions);
+      }*/
+    }
+  }
+
+  JS_DestroyContext(cx);
+  JS_ShutDown();
+  return 0;
+}
---
+++ third-party/.gclient
@@ -0,0 +1,10 @@
+solutions = [
+  { "name"        : 'v8',
+    "url"         : 'https://github.com/gilanghamidy/DifferentialFuzzingWASM-v8.git',
+    "deps_file"   : 'DEPS',
+    "managed"     : True,
+    "custom_deps" : {
+    },
+    "custom_vars": {},
+  },
+]
